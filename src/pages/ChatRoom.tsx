2c19b1491829dad832adbb9068a6caf6427975ad
swkoyo <10441291+swkoyo@users.noreply.github.com>
2023-01-02 12:32:03 +0900

    chatroom page init

--- src/pages/ChatRoom.tsx
+++ src/pages/ChatRoom.tsx
@@ -1,11 +1,24 @@
-import { Button, Input, Spinner, Text } from '@chakra-ui/react';
-import { MouseEvent, useEffect, useState } from 'react';
+import {
+    Box,
+    Button,
+    HStack,
+    Input,
+    InputGroup,
+    InputRightElement,
+    Spinner,
+    StackDivider,
+    Text,
+    VStack
+} from '@chakra-ui/react';
+import { capitalize } from 'lodash';
+import { MouseEvent, useEffect, useRef, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import useAuth from '../hooks/useAuth';
 import useMessages from '../hooks/useMessages';
 import MainContainer from '../layouts/MainContainer';
 import { sendMessage } from '../services/firebase';
 import { IUser } from '../types/auth';
+import { formatDate } from '../utils/dayjs';
 
 export default function ChatRoom() {
     const [value, setValue] = useState('');
@@ -13,6 +26,7 @@ export default function ChatRoom() {
     const { room_id } = useParams();
     const messages = useMessages(room_id as string);
     const [isLoading, setIsLoading] = useState(true);
+    const ref = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
         if (room_id) {
@@ -22,10 +36,16 @@ export default function ChatRoom() {
         }
     }, [room_id]);
 
+    useEffect(() => {
+        ref.current?.scrollIntoView();
+    }, [messages]);
+
     const handleSubmit = (e: MouseEvent<HTMLButtonElement>) => {
-        e.preventDefault();
-        sendMessage('general', user as IUser, value);
-        setValue('');
+        if (room_id) {
+            e.preventDefault();
+            sendMessage(room_id, user as IUser, value);
+            setValue('');
+        }
     };
 
     if (isLoading) {
@@ -38,14 +58,78 @@ export default function ChatRoom() {
 
     return (
         <MainContainer>
-            <Text>General</Text>
-            {messages.map((msg) => (
-                <Text key={msg.id}>{msg.text}</Text>
-            ))}
-            <Input value={value} onChange={(e) => setValue(e.target.value)} />
-            <Button type='button' onClick={handleSubmit}>
-                Submit
-            </Button>
+            <Text mb={10} alignSelf='center' fontSize='2xl' fontWeight='bold'>
+                {capitalize(room_id)}
+            </Text>
+            <Box
+                display='flex'
+                flexDir='column'
+                w='full'
+                border='1px'
+                borderRadius='xl'
+                height='600px'
+                justifyContent='end'
+            >
+                <VStack
+                    divider={<StackDivider />}
+                    alignItems='start'
+                    justifyContent='end'
+                    display='block'
+                    py={4}
+                    overflowY='scroll'
+                    sx={{
+                        '&::-webkit-scrollbar': {
+                            width: '10px',
+                            borderRadius: '8px',
+                            backgroundColor: `rgba(0, 0, 0, 0.05)`
+                        },
+                        '&::-webkit-scrollbar-thumb': {
+                            backgroundColor: 'whiteAlpha.300',
+                            borderRadius: '8px'
+                        }
+                    }}
+                >
+                    {messages.map((msg, i) => (
+                        <VStack
+                            alignItems='start'
+                            px={4}
+                            py={2}
+                            key={msg.id}
+                            ref={i === messages.length - 1 ? ref : null}
+                        >
+                            <HStack>
+                                <Text fontWeight='bold'>{msg.displayName}</Text>
+                                <Text fontSize='xs' fontWeight='thin'>
+                                    {msg.timestamp ? formatDate(msg.timestamp.toDate()) : 'just now'}
+                                </Text>
+                            </HStack>
+                            <Text>{msg.text}</Text>
+                        </VStack>
+                    ))}
+                </VStack>
+                <InputGroup pt={2}>
+                    <Input
+                        placeholder='Chat here'
+                        value={value}
+                        size='lg'
+                        variant='filled'
+                        onChange={(e) => setValue(e.target.value)}
+                        borderTopRadius='none'
+                        borderBottomRadius='lg'
+                        border='none'
+                        sx={{
+                            _focus: {
+                                background: 'whiteAlpha.100'
+                            }
+                        }}
+                    />
+                    <InputRightElement pr={2} width='4.5rem' pt={5}>
+                        <Button h='1.75rem' size='sm' justifySelf='center' type='button' onClick={handleSubmit}>
+                            Submit
+                        </Button>
+                    </InputRightElement>
+                </InputGroup>
+            </Box>
         </MainContainer>
     );
 }
--- src/types/messages.ts
+++ src/types/messages.ts
@@ -1,8 +1,8 @@
-import { FieldValue } from 'firebase/firestore';
+import { Timestamp } from 'firebase/firestore';
 import { IUser } from './auth';
 
 export interface IMessage extends IUser {
     id: string;
     text: string;
-    timestamp: FieldValue;
+    timestamp: Timestamp;
 }
